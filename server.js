3eaa16dac00754b80d8e113d3987cf16589ae145
Guadalupe Rocha <guadaluperocha@GuadalupesMBP2.attlocal.net>
2022-08-15 18:12:15 -0500

    connecting to mysql

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,19 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'Grocha@78',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 // default response for any other request (Not Found)
 app.use((req, res) => {
